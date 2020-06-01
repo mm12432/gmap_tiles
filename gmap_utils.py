529d592ec5fb7ad3dfd39254238c5d35184bc1fd
xiaoyong mao <43345113@qq.com>
2020-06-01 16:25:23 +0800

    迁移到python3 多level下载 全地球

--- gmap_utils.py
+++ gmap_utils.py
@@ -3,13 +3,17 @@
 
 import math
 
-def latlon2px(z,lat,lon):
-    x = 2**z*(lon+180)/360*256
-    y = -(.5*math.log((1+math.sin(math.radians(lat)))/(1-math.sin(math.radians(lat))))/math.pi-1)*256*2**(z-1)
-    return x,y
 
-def latlon2xy(z,lat,lon):
-    x,y = latlon2px(z,lat,lon)
-    x = int(x/256)#,int(x%256)
-    y = int(y/256)#,int(y%256)
-    return x,y
+def latlon2px(z, lat, lon):
+    x = 2**z * (lon + 180) / 360 * 256
+    y = -(.5 * math.log(
+        (1 + math.sin(math.radians(lat))) /
+        (1 - math.sin(math.radians(lat)))) / math.pi - 1) * 256 * 2**(z - 1)
+    return x, y
+
+
+def latlon2xy(z, lat, lon):
+    x, y = latlon2px(z, lat, lon)
+    x = int(x / 256)  #,int(x%256)
+    y = int(y / 256)  #,int(y%256)
+    return x, y
